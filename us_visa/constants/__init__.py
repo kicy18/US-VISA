f72128f91108170374fdaa589998c74fcb6d900e
kicy <80158936+kicy18@users.noreply.github.com>
2024-12-03 19:14:30 +0530

    prediction & app added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -19,6 +19,10 @@ CURRENT_YEAR = date.today().year
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 SCHEMA_FILE_PATH = os.path.join("config" , "schema.yaml")
 
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -56,3 +60,14 @@ MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisamodel2024"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
